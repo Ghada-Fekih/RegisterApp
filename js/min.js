cb27553e32a997627794bbeeb546cdf1cdd208c5
ghada-fekih <belfekih.ghada@gmail.com>
2023-11-20 22:08:13 +0100

    Essaie
    js

--- js/min.js
+++ js/min.js
@@ -1,2 +1,15 @@
-const nom = document.querySelector('.nom');
+const nom = document.querySelector ('.nom');
 console.log(nom.value);
+const submit = document.querySelector('.submit');
+function validateName(nom){
+    if(!nom){
+        return "name is required"
+    }
+}
+submit.addEventListener('click', (event) => {
+    if(validateName(nom.value) !== null){
+        alert(validateName(nom.value))
+    }
+
+})
+ 
