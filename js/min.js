3e531b2e6aefce709f0180d3dd92a5cff03a074f
Asma-Bk <148336119+Ghada-Fekih@users.noreply.github.com>
2023-11-21 10:42:14 +0100

    add html and css

--- js/min.js
+++ js/min.js
@@ -2,6 +2,7 @@ const n = document.querySelector(".nom");
 const submit = document.querySelector(".submit");
 const m = document.querySelector(".mail");
 
+
 submit.addEventListener('click', (event) => {
     console.log(n.value);
     console.log(m.value)
