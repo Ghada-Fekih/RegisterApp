d9f8fa6c4625df4d47d591e3450696ba35a9161e
Asma-Bk <148336119+Ghada-Fekih@users.noreply.github.com>
2023-11-21 12:07:18 +0100

    add js

--- js/min.js
+++ js/min.js
@@ -1,10 +1,36 @@
 const n = document.querySelector(".nom");
 const submit = document.querySelector(".submit");
-const m = document.querySelector(".mail");
-
+const email = document.querySelector(".email");
+const pw = document.querySelector(".password");
+const conP = document.querySelector(".conPasse")
+const error1 = document.querySelector(".error1");
+const error2 = document.querySelector(".error2");
+const error3 = document.querySelector(".error3");
+const error4 = document.querySelector(".error4");
+//test nom est vide
 
 submit.addEventListener('click', (event) => {
-    console.log(n.value);
-    console.log(m.value)
+  if (n.value == ""){
+      error1.innerHTML = `<span>nom est vide</span> `;
+      return;
+  }
+  else {error1.innerHTML = "";}
+  if (email.value == ""){
+    error2.innerHTML = `<span>mail est vide</span> `;
+    return;
+}
+else {error2.innerHTML = "";}
+if (pw.value == ""){
+    error3.innerHTML = `<span>Password est vide</span> `;
+    return;
+}
+if (!/^[0-9a-zA-z]+$/.test(pw)){
+    error3.innerHTML=`password doit etre des numero `;
+}
+else {error3.innerHTML = "";}
+if (conP.value != pw.value){
+    error4.innerHTML = `<span>Password nest pas conforme</span> `;
+return;}
+else {error4.innerHTML = "";}
 })
 
