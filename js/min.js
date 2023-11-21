771c4fdc2a5ea6b9f93013232108f033be4326b1
Asma-Bk <148336119+Ghada-Fekih@users.noreply.github.com>
2023-11-21 10:19:11 +0100

    Recuperer et afficher valeur des input
    add html et js

--- js/min.js
+++ js/min.js
@@ -1,15 +1,9 @@
-const nom = document.querySelector ('.nom');
-console.log(nom.value);
-const submit = document.querySelector('.submit');
-function validateName(nom){
-    if(!nom){
-        return "name is required"
-    }
-}
-submit.addEventListener('click', (event) => {
-    if(validateName(nom.value) !== null){
-        alert(validateName(nom.value))
-    }
+const n = document.querySelector(".nom");
+const submit = document.querySelector(".submit");
+const m = document.querySelector(".mail");
 
+submit.addEventListener('click', (event) => {
+    console.log(n.value);
+    console.log(m.value)
 })
- 
+
